0220acfec1dfd23cf7f15bd949219596ea97fc3c
HenryXu <92363852+hhenryxu@users.noreply.github.com>
2026-03-19 11:25:27 +0000

    1

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-1
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/", method = [ "GET" , "POST"])
+@app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
+
+@app.route("/main",methods=["GET","POST"])
+def main():
+    return(render_template("main.html"))
+
+@app.route("/transferMoney",methods=["GET","POST"])
+def transferMoney():
+    return(render_template("transferMoney.html"))
 
 if __name__ == "__main__":
     app.run()
